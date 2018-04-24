343d550f52996f5cd33539e8e689b19feab1e032
Lixingxing1992 <497655880@qq.com>
2018-04-24 19:23:23 +0800

    4.24

--- lib_common/src/main/java/com/app/org/utils/BaseAppUtil.java
+++ lib_common/src/main/java/com/app/org/utils/BaseAppUtil.java
@@ -647,6 +647,17 @@ public class BaseAppUtil {
     }
 
     /**
+     * 回到home，后台运行
+     */
+    public static void goHome(Context context) {
+        Intent mHomeIntent = new Intent(Intent.ACTION_MAIN);
+        mHomeIntent.addCategory(Intent.CATEGORY_HOME);
+        mHomeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        context.startActivity(mHomeIntent);
+    }
+
+    /**
      * 封装App信息的Bean类
      */
     public static class AppInfo {
