628ae6a34c508a60250a22fdcb170d3510d56807
Lixingxing1992 <497655880@qq.com>
2018-04-24 16:01:27 +0800

    4.24

--- lib_common/src/main/java/com/app/org/utils/BaseRouterUtil.kt
+++ lib_common/src/main/java/com/app/org/utils/BaseRouterUtil.kt
@@ -1,9 +1,10 @@
 package com.app.org.utils
 
-import android.app.Fragment
+import android.support.v4.app.Fragment
 import com.alibaba.android.arouter.launcher.ARouter
 
 /**
+ * 路由启动工具类
  * Created by lixingxing on 2018/4/23.
  */
 
@@ -13,6 +14,6 @@ fun startActivity(activityName:String) {
 }
 
 
-fun createFragment(fragmentName:String) :Fragment{
+fun createFragment(fragmentName:String) : Fragment {
     return ARouter.getInstance().build(fragmentName).navigation() as Fragment
 }
